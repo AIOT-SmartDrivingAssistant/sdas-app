fbefc9a2bcaf6d1070cb98af84281243da8875fe
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2025-05-15 00:22:55 +0700

    add dashboard

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,10 +3,12 @@ import activityHistory from '../pages/activityHistory'
 import Services from '../pages/Services'
 import Profile from '../pages/Profile'
 import Auth from '../pages/Auth'
+import DashBoard from '../pages/DashBoard'
 
 const publicRoutes = [
   {path: '/', component: Auth},
   {path: '/home', component: Home},
+  {path: '/dashboard', component: DashBoard},
   {path: '/history', component: activityHistory},
   {path: '/services', component: Services},
   {path: '/profile', component: Profile},
